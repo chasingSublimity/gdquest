381d19b912c0a2a1ab859c619216816600f33e88
Blake Sager <blakemsager@gmail.com>
2026-01-18 17:26:13 -0600

    Finish M7

--- 2d/M07.telling_a_story_workbook/lessons/dialogue.gd
+++ 2d/M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -2,9 +2,15 @@ extends Control
 
 @onready var body: TextureRect = %Body
 @onready var expression: TextureRect = %Expression
-@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
+@onready var sophia_voice: AudioStreamPlayer = %SophiaVoice
+@onready var pink_voice: AudioStreamPlayer = %PinkVoice
 @onready var next_button: Button = %NextButton
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
+
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
 
 var expressions: Dictionary[String, Resource] = {
 	"happy": preload("res://assets/emotion_happy.png"),
@@ -14,21 +20,31 @@ var expressions: Dictionary[String, Resource] = {
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions.regular,
-		"text": "Roses are red",
+		"text": "Roses are [wave]red[/wave]",
+		"character_name": "sophia"
 	},
 	{
 		"expression": expressions.sad,
 		"text": "Violets are blue",
+		"character_name": "pink"
 	},
 	{
 		"expression": expressions.regular,
 		"text": "This code is shite",
+		"character_name": "sophia"
 	},
 	{
 		"expression": expressions.happy,
 		"text": "And so are you!",
+		"character_name": "sophia"
 	}
 ]
+
+var speaking_voice: Dictionary[String, AudioStreamPlayer] = {
+	"sophia": sophia_voice,
+	"pink": pink_voice
+	
+}
 
 var current_item_index := 0
 
@@ -44,26 +60,34 @@ func advance() -> void:
 		show_text()
 
 func show_text() -> void:
-	var tween := create_tween()
 	var current_item := dialogue_items[current_item_index]
 	
-	rich_text_label.visible_ratio = 0.0
-	
+	var character_name = current_item.character_name
 	rich_text_label.text = current_item.text
 	expression.texture = current_item.expression
+	body.texture = bodies[character_name]
 	
-	var text_appearing_duration := 1.2
 	
+	var tween := create_tween()
+	var text_appearing_duration: float = rich_text_label.get_parsed_text().length() / 30.0
+	rich_text_label.visible_ratio = 0.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+	
+	var voice_player: AudioStreamPlayer = sophia_voice if character_name == "sophia" else pink_voice
 
-	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
+	var sound_max_offset := voice_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	
-	audio_stream_player.play(sound_start_position)
-	tween.finished.connect(audio_stream_player.stop)
+	voice_player.play(sound_start_position)
+	tween.finished.connect(voice_player.stop)
 	
 	# animate character appearance
 	slide_in()
+	
+	next_button.disabled = true
+	tween.finished.connect(func() -> void:
+		next_button.disabled = false
+	)
 
 func slide_in() -> void:
 	var tween := create_tween()
